8e842e68561070a4207d6f9dc79c7c07d60445b2
Victor Costan <costan@gmail.com>
2017-12-23 15:33:22 -0800

    More HTTP endpoints.

--- src/server/app.ts
+++ src/server/app.ts
@@ -1,10 +1,15 @@
-import * as Koa from "koa";
-import * as koaJson from "koa-json";
-import * as KoaRouter from "koa-router";
+import * as v8 from 'v8';
+
+import * as Koa from 'koa';
+import * as koaCors from '@koa/cors';
+import * as koaJson from 'koa-json';
+import * as KoaRouter from 'koa-router';
+
 import { readProfile } from '../db/profile';
 import readProfileMatches from '../jobs/read_profile_matches';
 import { readProfileMatchMetadata } from '../db/match_profile';
 import PagePool from '../cluster/page_pool';
+import { readMatch } from '../db/match';
 
 export const pagePool = new PagePool();
 
@@ -39,6 +44,14 @@ router.get('/profiles/:id', async (ctx, next) => {
   if (profile !== null)
     ctx.response.body = profile;
 });
+router.get('/matches/:id', async (ctx, next) => {
+  await next();
+
+  const matchId = ctx.params.id as string;
+  const match = await readMatch(matchId);
+  if (match !== null)
+    ctx.response.body = match;
+});
 router.get('/status/pool', async (ctx, next) => {
   await next();
 
@@ -47,7 +60,21 @@ router.get('/status/pool', async (ctx, next) => {
     urls: pagePool.pageWsUrls(),
   };
 });
+router.get('/status/memory', async (ctx, next) => {
+  await next();
+
+  ctx.response.body = {
+    process: process.memoryUsage(),
+    heap: v8.getHeapStatistics(),
+  };
+})
+router.get('/status', async (ctx, next) => {
+  await next();
+
+  ctx.response.body = 'OK';
+});
 
 export const app = new Koa();
+app.use(koaCors());
 app.use(koaJson({ pretty: false, param: 'pretty_json' }));
 app.use(router.routes()).use(router.allowedMethods());
